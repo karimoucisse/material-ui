ac0b743402e8ae406ab5866daca3c8aeb78e82a8
Karimou Cisse <karimou.cisse1@gmail.com>
2022-11-08 11:15:29 +0100

    [add] component folder

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
-import { Button, Typography } from "@mui/material"
-import { Box } from "@mui/system"
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import { Box } from "@mui/material"
+import Feed from "./components/Feed"
+import NavBar from "./components/NavBar"
+import RightBar from "./components/RightBar"
+import SideBar from "./components/SideBar"
+
 const App = () => {
   return (
-    <Box sx= {{
-      display: "flex", 
-      height: "100vh", 
-      justifyContent: "center", 
-      alignItems: "center"
-    }}>
-      <Button 
-        variant= "contained" 
-        color= "mainColor" 
-        size= "large"
-        endIcon= {<ThumbUpIcon/>}
-      >
-        Click me
-      </Button>
-      <Typography variant="p">hello world !</Typography>
-      <Button
-        variant= "contained"
-        sx= {{
-          backgroundColor: "lightblue",
-          color: "black",
-          "&:hover": {
-            backgroundColor: "skyblue"
-          }
-        }}
-      >
-        hello
-      </Button>
+    <Box>
+      {/* <NavBar/> */}
+      <SideBar/>
+      <Feed/>
+      <RightBar/>
     </Box>
   )
 }
