ff85a2e9cd9af72c54aca92acf094d4967173ab3
Karimou Cisse <karimou.cisse1@gmail.com>
2022-11-08 10:50:10 +0100

    [add] theme file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-
+import { Button, Typography } from "@mui/material"
+import { Box } from "@mui/system"
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 const App = () => {
   return (
-    <div>
-      app
-    </div>
+    <Box sx= {{
+      display: "flex", 
+      height: "100vh", 
+      justifyContent: "center", 
+      alignItems: "center"
+    }}>
+      <Button 
+        variant= "contained" 
+        color= "mainColor" 
+        size= "large"
+        endIcon= {<ThumbUpIcon/>}
+      >
+        Click me
+      </Button>
+      <Typography variant="p">hello world !</Typography>
+      <Button
+        variant= "contained"
+        sx= {{
+          backgroundColor: "lightblue",
+          color: "black",
+          "&:hover": {
+            backgroundColor: "skyblue"
+          }
+        }}
+      >
+        hello
+      </Button>
+    </Box>
   )
 }
 
