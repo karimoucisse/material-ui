2463a7d6b334585b2741eee4c02030ae9baed580
Karimou Cisse <karimou.cisse1@gmail.com>
2022-11-08 15:10:21 +0100

    [Add] imagesList component component

--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -1,13 +1,20 @@
 import { Box, Typography } from '@mui/material'
 import React from 'react'
 import Avatars from './Avatars'
+import ImagesList from './ImagesList'
 
 const RightBar = () => {
   return (
     <Box p={2} flex={2} sx= {{display: {xs: "none", sm: "block"}}}>
       <Box position="fixed">
-        <Typography variant= "h6" fontWeight={400}>Online friends</Typography>
-        <Avatars/>
+        <Box>
+          <Typography variant= "h6" fontWeight={400} sx= {{marginBottom: "10px"}}>Online friends</Typography>
+          <Avatars/>
+        </Box>
+        <Box>
+          <Typography variant= "h6" fontWeight={400} sx= {{marginBottom: "10px"}}>Latest photo</Typography>
+          <ImagesList/>
+        </Box>
       </Box>
     </Box>
   )
